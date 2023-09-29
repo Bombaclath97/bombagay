a8fbb7ba9c0c83f832bfa6fe461e8e430c3f6bb2
Bombaclath97 <e.bombardelli97@gmail.com>
2023-09-30 00:13:18 +0200

    test citazione

--- commands/citazione.js
+++ commands/citazione.js
@@ -3,21 +3,14 @@ module.exports = {
     description: 'citazione a caso',
     execute(onlineUsers, msg, args) {
         if (args.length === 0) {
-            msg.channel.send(`Come disse ${
-                onlineUsers[Math.floor(Math.random() * onlineUsers.length)].user.username
-            }, \"${
-                citazioni[Math.floor(Math.random() * citazioni.length)]
-            }\"`);
+            const username = onlineUsers[Math.floor(Math.random() * onlineUsers.length)].user.username
+            const result = getCitazione(username)
+            msg.channel.send(`${result.citazione}`)
         }
     }
 }
 
-const citazioni = [
-    'se io sono ciò che è nei miei pantaloni, io sono cacca',
-    'due cose sono infinite in questo universo: la stupidità umana ed il fetillo di mamm\'t',
-    'quindi niente pompino di cittadinanza? Aggio capito, addios',
-    'ste femmn ladr so comm o café: tutt puttan',
-    'le femmine mi piacciono come mi piace la pizza: senza il cazzo di qualcuno sopra',
-    'chiavare con un uomo è l\'unica cosa alfa che puoi fare, perché fare sesso con le femmine è da gay',
-    'sì sono FELICE:\nForse\nÈ\niL caso di\nChiamare i carabinieri perché sono pazzo in culo\nE'
-];
+const getCitazione = async (username) => {
+    const response = await fetch(`${process.env.BACKEND_URL}/citazione?username=${username}`)
+    return await response.json()
+}

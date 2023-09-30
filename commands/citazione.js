7113105d0cca7c96f0ee24371ab428533c73d062
Bombaclath97 <e.bombardelli97@gmail.com>
2023-09-30 02:05:14 +0200

    Check body

--- commands/citazione.js
+++ commands/citazione.js
@@ -1,18 +1,23 @@
 module.exports = {
-    name: 'citazione',
-    description: 'citazione a caso',
-    execute(onlineUsers, msg, args) {
-        if (args.length === 0) {
-            const username = onlineUsers[Math.floor(Math.random() * onlineUsers.length)].user.username
-            const result = getCitazione(username)
-            console.log(result)
-            msg.channel.send(`${result.citazione}`)
-        }
+  name: "citazione",
+  description: "citazione a caso",
+  execute(onlineUsers, msg, args) {
+    if (args.length === 0) {
+      const username =
+        onlineUsers[Math.floor(Math.random() * onlineUsers.length)].user
+          .username;
+      getCitazione(username)
+        .then((result) => msg.channel.send(`${result.citazione}`))
+        .catch((reason) => console.log(reason));
+      console.log(result);
     }
-}
+  },
+};
 
 const getCitazione = async (username) => {
-    const response = await fetch(`http://${process.env.BACKEND_URL}/citazione?username=${username}`)
-    const json = await response.json()
-    return json
-}
+  const response = await fetch(
+    `http://${process.env.BACKEND_URL}/citazione?username=${username}`
+  );
+  const json = await response.json();
+  return json;
+};
